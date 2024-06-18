5e5a90192951a73566af3a05edd5757b0892f907
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-06-19 00:24:34 +0530

    Corrected the logic of lorem ipsum generator

--- Daily-Dev-Tools/src/helper-Tool-components/input-slider.tsx
+++ Daily-Dev-Tools/src/helper-Tool-components/input-slider.tsx
@@ -1,24 +1,65 @@
-import { HStack, Input, Slider, SliderFilledTrack, SliderThumb, SliderTrack, VStack } from "@chakra-ui/react"
+import { HStack, Input, Slider, SliderFilledTrack, SliderMark, SliderThumb, SliderTrack } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
 
 interface Props {
     placeholder: string,
     minVal: number,
     maxVal: number,
-    step: number
+    step: number,
+    value: number,
+    onChange: (value: number) => void
 }
 
-const InputSlider = ({ placeholder, minVal, maxVal, step }: Props) => {
+const InputSlider = ({ placeholder, minVal, maxVal, step, value, onChange }: Props) => {
+    const [inputValue, setInputValue] = useState(value);
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newValue = Number(e.target.value);
+        if (newValue >= minVal && newValue <= maxVal) {
+            setInputValue(newValue);
+            onChange(newValue);
+        }
+    };
+
+    const handleSliderChange = (value: number) => {
+        setInputValue(value);
+        onChange(value);
+    };
+
     return (
         <HStack spacing={5}>
-            <Input placeholder={placeholder} size="xs" />
-            <Slider defaultValue={1} min={minVal} max={maxVal} step={step}>
-                <SliderTrack bg='red.100'>
+            <Input
+                placeholder={placeholder}
+                size="xs"
+                value={inputValue <= maxVal ? inputValue : maxVal}
+                onChange={handleInputChange}
+                focusBorderColor="white"
+            />
+            <Slider
+                min={minVal}
+                max={maxVal}
+                step={step}
+                value={inputValue}
+                onChange={handleSliderChange}
+            >
+                <SliderMark
+                    value={inputValue <= maxVal ? inputValue : maxVal}
+                    textAlign='center'
+                    bg='blue.500'
+                    color='white'
+                    mt='-10'
+                    ml='-4'
+                    w='8'
+                >
+                    {inputValue <= maxVal ? inputValue : maxVal}
+                </SliderMark>
+                <SliderTrack bg='white'>
                     <SliderFilledTrack bg='tomato' />
                 </SliderTrack>
                 <SliderThumb boxSize={5} />
             </Slider>
         </HStack>
-    )
-}
+    );
+};
 
-export default InputSlider
+export default InputSlider;
